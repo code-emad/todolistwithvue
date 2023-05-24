78938a51f673d5d3543f3d626b7c78bab3a223b5
Emad <emadul.islam@hotmail.co.uk>
2023-05-24 12:55:07 +0100

    creating input box

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <script setup>
-import {ref, onMounted, computed, watch} from 'vue'
+import { ref, onMounted, computed, watch } from "vue";
 
-const todos = ref([])
-const name = ref('')
+const todos = ref([]);
+const name = ref("");
 
-const input_content = ref('')
-const input_catergory = ref(null)
+const input_content = ref("");
+const input_catergory = ref(null);
 
-const todos_ascending = computed(() => todos.value.sort((a, b) => {
-  return b.createdAt - a.createdAt
-}))
+const todos_ascending = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
 </script>
 
 <template>
-<div>
-  Hello World
-</div>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+  </main>
 </template>
-
