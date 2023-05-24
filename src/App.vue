237510c2d4ce87c831b6fb207a8316aba05abae6
Emad <emadul.islam@hotmail.co.uk>
2023-05-24 12:50:27 +0100

    creating variables

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup>
+import {ref, onMounted, computed, watch} from 'vue'
 
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_catergory = ref(null)
+
+const todos_ascending = computed(() => todos.value.sort((a, b) => {
+  return b.createdAt - a.createdAt
+}))
 </script>
 
 <template>
